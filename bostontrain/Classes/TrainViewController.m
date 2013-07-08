18d84650d1b11c4012af9558c39603bdd9c00513
Alex Liu <zezhouliu@college.harvard.edu>
2013-07-07 17:56:33 -0700

    Moved to TrainViewController

--- bostontrain/Classes/TrainViewController.m
+++ bostontrain/Classes/TrainViewController.m
@@ -42,4 +42,73 @@
     // Dispose of any resources that can be recreated.
 }
 
+# pragma mark - Buttons
+- (FUIButton *) realTimeButton
+{
+    if (_realTimeButton) {
+        _realTimeButton = [[FUIButton alloc] init];
+        _realTimeButton.buttonColor = self.buttonColor;
+        _realTimeButton.shadowColor = self.buttonShadowColor;
+        _realTimeButton.shadowHeight = 5.0f;
+        _realTimeButton.cornerRadius = 5.0f;
+        _realTimeButton.titleLabel.font = [UIFont boldFlatFontOfSize:16];
+        [_realTimeButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateNormal];
+        [_realTimeButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateHighlighted];
+        
+    }
+    
+    return _realTimeButton;
+}
+
+- (FUIButton *) nextArrivalButton
+{
+    if (_nextArrivalButton) {
+        _nextArrivalButton = [[FUIButton alloc] init];
+        _nextArrivalButton.buttonColor = self.buttonColor;
+        _nextArrivalButton.shadowColor = self.buttonShadowColor;
+        _nextArrivalButton.shadowHeight = 5.0f;
+        _nextArrivalButton.cornerRadius = 5.0f;
+        _nextArrivalButton.titleLabel.font = [UIFont boldFlatFontOfSize:16];
+        [_nextArrivalButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateNormal];
+        [_nextArrivalButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateHighlighted];
+        
+    }
+    
+    return _nextArrivalButton;
+}
+
+- (FUIButton *) scheduleButton
+{
+    if (_scheduleButton) {
+        _scheduleButton = [[FUIButton alloc] init];
+        _scheduleButton.buttonColor = self.buttonColor;
+        _scheduleButton.shadowColor = self.buttonShadowColor;
+        _scheduleButton.shadowHeight = 5.0f;
+        _scheduleButton.cornerRadius = 5.0f;
+        _scheduleButton.titleLabel.font = [UIFont boldFlatFontOfSize:16];
+        [_scheduleButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateNormal];
+        [_scheduleButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateHighlighted];
+        
+    }
+    
+    return _scheduleButton;
+}
+
+- (FUIButton *) mapButton
+{
+    if (_mapButton) {
+        _mapButton = [[FUIButton alloc] init];
+        _mapButton.buttonColor = self.buttonColor;
+        _mapButton.shadowColor = self.buttonShadowColor;
+        _mapButton.shadowHeight = 5.0f;
+        _mapButton.cornerRadius = 5.0f;
+        _mapButton.titleLabel.font = [UIFont boldFlatFontOfSize:16];
+        [_mapButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateNormal];
+        [_mapButton setTitleColor:[UIColor cloudsColor] forState:UIControlStateHighlighted];
+        
+    }
+    
+    return _mapButton;
+}
+
 @end
